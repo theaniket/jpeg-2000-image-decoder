b6d6f333cc22892a8dcc01e6a8a1230950eeb669
aniket <aniket.chadha9@gmail.com>
2018-07-26 16:14:36 +0530

    second commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using CSJ2K.Util;
+using System;
+using System.IO;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // from byte array
+            byte[] imageDataInBinary = File.ReadAllBytes(@"C:\Users\aniketchadha\Pictures\v.jp2");
+            MemoryStream memoryStreamObject = new MemoryStream();
+            memoryStreamObject.Write(imageDataInBinary, 0, imageDataInBinary.Length);
+            BitmapImageCreator.Register();
+
+            // from file
+            CSJ2K.Util.PortableImage portableImageObject = CSJ2K.J2kImage.FromFile(@"D:\Jpeg2k.Samples-master\Jpeg2k.Samples-master\Samples\Sample Data\balloon.j2c");
+            try
+            {
+                var img = portableImageObject.As<System.Drawing.Image>();
+                img.Save(@"C:\Users\aniketchadha\Pictures\balloon2.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
